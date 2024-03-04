bd2f37fe5ae4071ea3461a7e471beea46cabc497
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 15:06:59 +0300

    Created the workspaced header

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -1,7 +1,10 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
 import { useOrganization, useOrganizationList } from '@clerk/nextjs'
+import { Plus } from 'lucide-react'
+import Link from 'next/link'
 import { useLocalStorage } from 'usehooks-ts'
 
 type Props = {
@@ -46,7 +49,24 @@ const Sidebar = ({ storageKey = 't-sidebar-state' }: Props) => {
 		)
 	}
 
-	return <div>Sidebar</div>
+	return (
+		<>
+			<div className='font-medium text-sx flex items-center mb-1'>
+				<span className='pl-4'>Workspaces</span>
+				<Button
+					asChild
+					type='button'
+					size='icon'
+					variant='ghost'
+					className='ml-auto'
+				>
+					<Link href={'/select-org'}>
+						<Plus className='h-4 w-4' />
+					</Link>
+				</Button>
+			</div>
+		</>
+	)
 }
 
 export default Sidebar
