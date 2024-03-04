eb991a53b0a5c9e2eb392bd8133357e226e06161
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 14:37:57 +0300

    Added some logic to control the organizations and their parameters

--- app/(platform)/(dashboard)/organization/[id]/_components/OrgControl.tsx
+++ app/(platform)/(dashboard)/organization/[id]/_components/OrgControl.tsx
@@ -1,7 +1,24 @@
+'use client'
+
+import { useOrganizationList } from '@clerk/nextjs'
+import { useParams } from 'next/navigation'
+import { useEffect } from 'react'
+
 type Props = {}
 
 const OrgControl = (props: Props) => {
-	return <div>OrgControl</div>
+	const { id } = useParams()
+	const { setActive } = useOrganizationList()
+
+	useEffect(() => {
+		if (!setActive) return
+
+		setActive({
+			organization: id as string
+		})
+	}, [setActive, id])
+
+	return null
 }
 
 export default OrgControl
