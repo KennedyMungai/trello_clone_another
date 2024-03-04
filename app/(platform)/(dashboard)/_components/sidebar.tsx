7b171f072062e7f98fde002eb714569dd30dc804
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 15:03:04 +0300

    Loaded up a skeleton when the org info is incomplete

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -1,10 +1,51 @@
 'use client'
 
+import { Skeleton } from '@/components/ui/skeleton'
+import { useOrganization, useOrganizationList } from '@clerk/nextjs'
+import { useLocalStorage } from 'usehooks-ts'
+
 type Props = {
 	storageKey?: string
 }
 
-const Sidebar = ({ storageKey }: Props) => {
+const Sidebar = ({ storageKey = 't-sidebar-state' }: Props) => {
+	const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+		storageKey,
+		{}
+	)
+
+	const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+		useOrganization()
+
+	const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+		userMemberships: {
+			infinite: true
+		}
+	})
+
+	const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+		(acc: string[], key: string) => {
+			if (expanded[key]) {
+				acc.push()
+			}
+
+			return acc
+		},
+		[]
+	)
+
+	const onExpand = (id: string) => {
+		setExpanded((curr) => ({ ...curr, [id]: !expanded[id] }))
+	}
+
+	if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+		return (
+			<>
+				<Skeleton />
+			</>
+		)
+	}
+
 	return <div>Sidebar</div>
 }
 
