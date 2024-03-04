357e9465a5c1de7466085d1053099d67ff34d88a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 14:21:03 +0300

    Added more styles for the main tag of the OrganizationsLayout

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -5,7 +5,7 @@ type Props = {
 }
 
 const OrganizationsLayout = ({ children }: Props) => {
-	return <main className='pt-20'>{children}</main>
+	return <main className='pt-20 md:pt-24 px-4 max-w-6xl'>{children}</main>
 }
 
 export default OrganizationsLayout
