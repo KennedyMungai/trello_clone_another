b8eae6894cf272b99cd693f7376dc15763c838bb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 11:18:22 +0300

    Added some styles for the index page of the application

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,12 @@
-import { MedalIcon } from 'lucide-react'
+import { Medal } from 'lucide-react'
 
 export default function Home() {
 	return (
 		<main className='flex items-center justify-center flex-col'>
 			<div className='flex items-center justify-center flex-col'>
-				<div className=''>
-					<MedalIcon />
+				<div className='mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase'>
+					<Medal className='h-6 w-6 mr-2' />
+					No 1 Task Management
 				</div>
 			</div>
 		</main>
