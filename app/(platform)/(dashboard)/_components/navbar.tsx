2f55d5d28968ebf6a138f979cd6b8d301d66fe8d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 13:00:22 +0300

    Added some padding for the NavBat

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const NavBar = (props: Props) => {
 	return (
-		<div className='fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center'>
+		<div className='fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center px-4'>
 			{/* TODO: Mobile Sidebar */}
 			<div className='flex items-center gap-x-4'>
 				<div className='hidden md:flex'>
