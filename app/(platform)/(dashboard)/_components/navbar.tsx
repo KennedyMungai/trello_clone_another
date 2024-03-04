d0a1e55746a69ee09fa3c96c33e782e212a25510
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 12:59:41 +0300

    Rendered out the Logo component

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,7 +1,18 @@
+import Logo from '@/components/logo'
+
 type Props = {}
 
 const NavBar = (props: Props) => {
-	return <div>NavBar</div>
+	return (
+		<div className='fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center'>
+			{/* TODO: Mobile Sidebar */}
+			<div className='flex items-center gap-x-4'>
+				<div className='hidden md:flex'>
+					<Logo />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default NavBar
