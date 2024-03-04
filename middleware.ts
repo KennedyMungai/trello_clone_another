7ace4a8f8d27d2bccfa21e3f42a633f909da23e1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 13:14:55 +0300

    Added some redirection logic to the middleware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,23 @@
 import { authMiddleware } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
 
 export default authMiddleware({
-	publicRoutes: ['/']
+	publicRoutes: ['/'],
+	afterAuth(auth, req) {
+		if (auth?.userId && auth.isPublicRoute) {
+			let path = '/select-org'
+
+			if (auth.orgId) {
+				path = `/organization/${auth.orgId}`
+			}
+
+			const orgSelection = new URL(path, req.url)
+
+			return NextResponse.redirect(orgSelection)
+		}
+	}
 })
 
 export const config = {
-	// Protects all routes, including api/trpc.
-	// See https://clerk.com/docs/references/nextjs/auth-middleware
-	// for more information about configuring your Middleware
 	matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)']
 }
