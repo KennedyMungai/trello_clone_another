e4920a2e72c8f0d33929a8e4c3069d730aeda57e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 16:21:19 +0300

    Added a sheet that opens and closes based on the zustand state

--- app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,11 +1,47 @@
 'use client'
 
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Sheet, SheetContent } from '@/components/ui/sheet'
+import { useMobileSidebar } from '@/hooks/use-mobile-sidebar'
+import { Menu } from 'lucide-react'
+import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import Sidebar from './sidebar'
 
 type Props = {}
 
 const MobileSidebar = (props: Props) => {
-	return <div>MobileSidebar</div>
+	const { isOpen, onClose, onOpen } = useMobileSidebar((state) => state)
+	const pathname = usePathname()
+	const [isMounted, setIsMounted] = useState(false)
+
+	useEffect(() => {
+		setIsMounted(true)
+	}, [])
+
+	useEffect(() => {
+		onClose()
+	}, [pathname, onClose])
+
+	if (!isMounted) return
+
+	return (
+		<>
+			<Button
+				size='sm'
+				variant='outline'
+				onClick={onOpen}
+				className={'block md:hidden'}
+			>
+				<Menu className='h-4 w-4' />
+			</Button>
+			<Sheet open={isOpen} onOpenChange={onClose}>
+				<SheetContent className='p-2 pt-10'>
+					<Sidebar storageKey='t-sidebar-mobile-state' />
+				</SheetContent>
+			</Sheet>
+		</>
+	)
 }
 
 export default MobileSidebar
