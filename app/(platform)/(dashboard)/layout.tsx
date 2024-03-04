8c3d9cde241971bf4d4236b5fbc3bb118e4e8b89
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 12:56:31 +0300

    Created and rendered a navbar component

--- app/(platform)/(dashboard)/layout.tsx
+++ app/(platform)/(dashboard)/layout.tsx
@@ -1,11 +1,17 @@
 import { ReactNode } from 'react'
+import NavBar from './_components/navbar'
 
 type Props = {
 	children: ReactNode
 }
 
 const OrganizationLayout = ({ children }: Props) => {
-	return <div>{children}</div>
+	return (
+		<div className='h-full'>
+			<NavBar />
+			{children}
+		</div>
+	)
 }
 
 export default OrganizationLayout
