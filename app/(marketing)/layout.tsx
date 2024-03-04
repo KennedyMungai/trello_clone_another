63ab3bd155ca837f2767d90e5701bd3fdba78eac
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 11:35:48 +0300

    Set the marketting folder to use popins

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,4 +1,11 @@
+import { cn } from '@/lib/utils'
+import { Poppins } from 'next/font/google'
 import { ReactNode } from 'react'
+
+const poppins = Poppins({
+	subsets: ['latin'],
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+})
 
 type Props = {
 	children: ReactNode
@@ -7,9 +14,11 @@ type Props = {
 const MarketingLayout = ({ children }: Props) => {
 	return (
 		<div className='h-full bg-slate-100'>
-            {/* TODO: Navbar */}
-			<main className='pt-40 pb-20 bg-slate-100'>{children}</main>
-            {/* TODO: Footer */}
+			{/* TODO: Navbar */}
+			<main className={cn('pt-40 pb-20 bg-slate-100', poppins.className)}>
+				{children}
+			</main>
+			{/* TODO: Footer */}
 		</div>
 	)
 }
