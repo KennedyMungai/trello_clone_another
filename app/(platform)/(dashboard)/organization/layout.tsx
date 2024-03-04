b16cdefaf10e1e9103d22c38194a73000bd63fb9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 14:23:27 +0300

    Added some styles to hide the sidebar on the small screens

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -7,7 +7,9 @@ type Props = {
 const OrganizationsLayout = ({ children }: Props) => {
 	return (
 		<main className='pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto'>
-			{children}
+			<div className='flex gap-x-7'>
+				<div className='w-64 shrink-0 hidden md:block'>{children}</div>
+			</div>
 		</main>
 	)
 }
