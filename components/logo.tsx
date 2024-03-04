80202302e567237f19094c3ba16cdf833cfa44ba
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 11:47:08 +0300

    Updated the name of the image in the Logo componennt

--- components/logo.tsx
+++ components/logo.tsx
@@ -7,7 +7,7 @@ const Logo = (props: Props) => {
 	return (
 		<Link href={'/'}>
 			<div className='hover:opacity-75 transition items-center gap-x-2 hidden md:flex'>
-				<Image src={'logo.svg'} alt='Logo' width={30} height={30} />
+				<Image src={'trello.svg'} alt='Logo' width={30} height={30} />
 				<p className='text-lg text-neutral-700 pb-1'>Trello Clone</p>
 			</div>
 		</Link>
