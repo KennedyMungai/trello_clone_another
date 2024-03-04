32875b13ae56824fef945ffb4f892d64ab19a95a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 15:46:26 +0300

    Rendered out the proper icons and labels for the buttons

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -1,9 +1,15 @@
 'use client'
 
-import { AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
+import {
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger
+} from '@/components/ui/accordion'
+import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
-import { Activity, Layout, Settings } from 'lucide-react'
+import { Activity, CreditCard, Layout, Settings } from 'lucide-react'
 import Image from 'next/image'
+import { usePathname, useRouter } from 'next/navigation'
 
 export type Organization = {
 	id: string
@@ -20,23 +26,35 @@ type Props = {
 }
 
 const NavItem = ({ isActive, isExpanded, onExpand, organization }: Props) => {
+	const router = useRouter()
+	const pathname = usePathname()
+
 	const routes = [
 		{
 			label: 'Boards',
 			icon: <Layout className='h-4 w-4 mr-2' />,
 			href: `/organization/${organization.id}`
 		},
-        {
-            label: 'Activity',
-            icon: <Activity className="h-4 w-4 mr-2" />,
-            href: `/organization/${organization.id}/activity`
-        },
-        {
-            label: 'Settings',
-            icon: <Settings className="h-4 w-4 mr-2" />,
-            href: `/organization/${organization.id}/settings`
-        }
+		{
+			label: 'Activity',
+			icon: <Activity className='h-4 w-4 mr-2' />,
+			href: `/organization/${organization.id}/activity`
+		},
+		{
+			label: 'Settings',
+			icon: <Settings className='h-4 w-4 mr-2' />,
+			href: `/organization/${organization.id}/settings`
+		},
+		{
+			label: 'Billing',
+			icon: <CreditCard className='h-4 w-4 mr-2' />,
+			href: `/organization/${organization.id}/billing`
+		}
 	]
+
+	const onClick = (href: string) => {
+		router.push(href)
+	}
 
 	return (
 		<AccordionItem value={organization.id} className='border-none'>
@@ -61,6 +79,21 @@ const NavItem = ({ isActive, isExpanded, onExpand, organization }: Props) => {
 					</span>
 				</div>
 			</AccordionTrigger>
+			<AccordionContent className='pt-1 text-neutral-700'>
+				{routes.map((route, index) => (
+					<Button
+						key={index}
+						size='sm'
+						onClick={() => onClick(route.href)}
+						className={cn(
+							'w-full font-normal justify-start pl-10 mb-1',
+							pathname === route.href &&
+								'bg-sky-500/10 text-sky-700'
+						)}
+                        variant={'ghost'}
+					>{route.icon} {route.label}</Button>
+				))}
+			</AccordionContent>
 		</AccordionItem>
 	)
 }
