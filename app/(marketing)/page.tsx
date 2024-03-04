2431a60caf5c1cfbcfe92c543b1373f5a94a4861
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 11:23:05 +0300

    Added some text to the main page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -8,6 +8,12 @@ export default function Home() {
 					<Medal className='h-6 w-6 mr-2' />
 					No 1 Task Management
 				</div>
+				<h1 className='text-3xl md:text-6xl text-center text-neutral-800 mb-6'>
+					Trello Clone Helps Teams Move
+				</h1>
+				<div className='text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-800 to-pink-600 text-transparent bg-clip-text'>
+					Work Forward.
+				</div>
 			</div>
 		</main>
 	)
