137ff3ec21b713c27cea3354ababf5e24282f712
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 11:15:58 +0300

    Added some more markup

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,13 @@
+import { MedalIcon } from 'lucide-react'
+
 export default function Home() {
 	return (
 		<main className='flex items-center justify-center flex-col'>
-			Marketing Page
+			<div className='flex items-center justify-center flex-col'>
+				<div className=''>
+					<MedalIcon />
+				</div>
+			</div>
 		</main>
 	)
 }
