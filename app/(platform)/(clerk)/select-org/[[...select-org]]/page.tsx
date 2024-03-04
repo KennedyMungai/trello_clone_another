f898d7749a6fdd43ebf7902b74122cf9a35ce0b4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 12:33:54 +0300

    Created the page to select an organization

---
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -0,0 +1,9 @@
+import { OrganizationList } from '@clerk/nextjs'
+
+type Props = {}
+
+const SelectOrganization = (props: Props) => {
+	return <OrganizationList />
+}
+
+export default SelectOrganization
