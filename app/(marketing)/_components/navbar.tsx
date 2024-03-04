3f1f317959f6c665add9db375b7631bd6016c766
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 11:46:24 +0300

    Rendered out the Logo component

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,7 +1,15 @@
+import Logo from '@/components/logo'
+
 type Props = {}
 
 const NavBar = (props: Props) => {
-	return <nav>NavBar</nav>
+	return (
+		<nav className='fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center'>
+			<div className='md:max-w-screen-2xl mx-auto flex items-center w-full justify-between'>
+				<Logo />
+			</div>
+		</nav>
+	)
 }
 
 export default NavBar
