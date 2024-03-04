0f0604309619bf5ab701170efb1505725c736ece
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 12:52:17 +0300

    Dropped all extra code

--- app/(platform)/(dashboard)/organization/[id]/page.tsx
+++ app/(platform)/(dashboard)/organization/[id]/page.tsx
@@ -1,16 +1,7 @@
-import { auth } from '@clerk/nextjs'
-
 type Props = {}
 
 const SingleOrganizationPage = (props: Props) => {
-	const { userId, orgId } = auth()
-
-	return (
-		<div>
-			User Id: {userId}
-			Organization: {orgId}
-		</div>
-	)
+	return <div>SingleOrganizationPage</div>
 }
 
 export default SingleOrganizationPage
