0df9c5a86465a64c65342a5722aadd3bff1ed0c6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 16:08:36 +0300

    Mobile Sidebar shenanigans

---
+++ app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React from 'react'
+
+type Props = {}
+
+const MobileSidebar = (props: Props) => {
+	return <div>MobileSidebar</div>
+}
+
+export default MobileSidebar
--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -2,13 +2,14 @@ import Logo from '@/components/logo'
 import { Button } from '@/components/ui/button'
 import { OrganizationSwitcher, UserButton } from '@clerk/nextjs'
 import { Plus } from 'lucide-react'
+import MobileSidebar from './mobile-sidebar'
 
 type Props = {}
 
 const NavBar = (props: Props) => {
 	return (
 		<nav className='fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center px-4'>
-			{/* TODO: Mobile Sidebar */}
+            <MobileSidebar />
 			<div className='flex items-center gap-x-4'>
 				<div className='hidden md:flex'>
 					<Logo />
