7c87ffc425bba600258c40b3c133ff6f45fac0f2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-04 13:00:51 +0300

    Made some markup changes

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -4,14 +4,14 @@ type Props = {}
 
 const NavBar = (props: Props) => {
 	return (
-		<div className='fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center px-4'>
+		<nav className='fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center px-4'>
 			{/* TODO: Mobile Sidebar */}
 			<div className='flex items-center gap-x-4'>
 				<div className='hidden md:flex'>
 					<Logo />
 				</div>
 			</div>
-		</div>
+		</nav>
 	)
 }
 
