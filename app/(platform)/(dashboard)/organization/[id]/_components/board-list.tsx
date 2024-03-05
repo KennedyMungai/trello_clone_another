cab51ef47dc8504da757a201851675a21c686541
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 14:56:28 +0300

    Created a board to be used for creating new boards

--- app/(platform)/(dashboard)/organization/[id]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[id]/_components/board-list.tsx
@@ -9,6 +9,17 @@ const BoardList = (props: Props) => {
 				<User2 className='h-6 w-6 mr-2' />
 				Your Boards
 			</div>
+			<div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-'>
+				<div
+					role='button'
+					className='aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition'
+				>
+					<p className='text-sm'>Create New Board</p>
+					<span className='text-xs text-muted-foreground'>
+						5 remaining
+					</span>
+				</div>
+			</div>
 		</div>
 	)
 }
