f874221fff96f06fefa3531e39dd10cabdb04ee4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 14:32:48 +0300

    Rendered out the image of the currently selected organization

--- app/(platform)/(dashboard)/organization/[id]/_components/info.tsx
+++ app/(platform)/(dashboard)/organization/[id]/_components/info.tsx
@@ -1,9 +1,29 @@
 'use client'
 
+import { useOrganization } from '@clerk/nextjs'
+import Image from 'next/image'
+
 type Props = {}
 
 const Info = (props: Props) => {
-	return <div>Info</div>
+	const { organization, isLoaded } = useOrganization()
+
+	if (!isLoaded) {
+		return <p>Loading...</p>
+	}
+
+	return (
+		<div className='flex items-center gap-x-4'>
+			<div className='w-[60px] h-[60px] relative'>
+				<Image
+					fill
+					src={organization!.imageUrl}
+					alt='organization'
+					className='rounded-md object-cover'
+				/>
+			</div>
+		</div>
+	)
 }
 
 export default Info
