ac923cacd439ab5401b84796b21eac8df64acd5c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 16:08:18 +0300

    Created the formpicker component

---
+++ components/form/form-picker.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+
+type Props = {
+	id: string
+	errors?: Record<string, string[] | undefined>
+}
+
+const FormPicker = ({ id, errors }: Props) => {
+	return <div>FormPicker</div>
+}
+
+export default FormPicker
