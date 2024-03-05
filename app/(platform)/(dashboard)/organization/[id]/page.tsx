00792971c94e42b36cb731750fab493a19fdc9c9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 09:36:02 +0300

    Added an input field on the board page

--- app/(platform)/(dashboard)/organization/[id]/page.tsx
+++ app/(platform)/(dashboard)/organization/[id]/page.tsx
@@ -1,7 +1,19 @@
 type Props = {}
 
 const SingleOrganizationPage = (props: Props) => {
-	return <div>SingleOrganizationPage</div>
+	return (
+		<div className=''>
+			<form>
+				<input
+					id='title'
+					name='title'
+					required
+					placeholder='Enter a board title'
+					className='border-black border p-1'
+				/>
+			</form>
+		</div>
+	)
 }
 
 export default SingleOrganizationPage
