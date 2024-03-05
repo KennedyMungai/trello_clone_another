8d8aa6aae38248fdd822b6423c91a6299bade3ce
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 14:27:12 +0300

    Created and rendered the Info commponent

---
+++ app/(platform)/(dashboard)/organization/[id]/_components/info.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+type Props = {}
+
+const Info = (props: Props) => {
+	return <div>Info</div>
+}
+
+export default Info
--- app/(platform)/(dashboard)/organization/[id]/page.tsx
+++ app/(platform)/(dashboard)/organization/[id]/page.tsx
@@ -1,5 +1,9 @@
+import Info from "./_components/info"
+
 const SingleOrganizationPage = async () => {
-	return <div className='w-full mb-2'></div>
+	return <div className='w-full mb-2'>
+		<Info />
+	</div>
 }
 
 export default SingleOrganizationPage
