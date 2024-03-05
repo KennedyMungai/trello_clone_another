50e8a5d6ffd939997cd6ea63405527062afeb7b4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 14:45:54 +0300

    Created and rendered out the BoardList component

---
+++ app/(platform)/(dashboard)/organization/[id]/_components/board-list.tsx
@@ -0,0 +1,7 @@
+type Props = {}
+
+const BoardList = (props: Props) => {
+	return <div>BoardList</div>
+}
+
+export default BoardList
--- app/(platform)/(dashboard)/organization/[id]/page.tsx
+++ app/(platform)/(dashboard)/organization/[id]/page.tsx
@@ -1,11 +1,17 @@
-import { Separator } from "@/components/ui/separator"
-import Info from "./_components/info"
+import { Separator } from '@/components/ui/separator'
+import Info from './_components/info'
+import BoardList from './_components/board-list'
 
 const SingleOrganizationPage = async () => {
-	return <div className='w-full mb-2'>
-		<Info />
-		<Separator className="my-4" />
-	</div>
+	return (
+		<div className='w-full mb-2'>
+			<Info />
+			<Separator className='my-4' />
+			<div className='px-2 md:px-4'>
+				<BoardList />
+			</div>
+		</div>
+	)
 }
 
 export default SingleOrganizationPage
