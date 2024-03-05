18625a6e9652e3cbfd4e00de8d5651942261fc31
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 10:19:04 +0300

    Made some changes to the form component

--- app/(platform)/(dashboard)/organization/[id]/form.tsx
+++ app/(platform)/(dashboard)/organization/[id]/form.tsx
@@ -1,13 +1,16 @@
 'use client'
 import { create } from '@/actions/create-board'
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import { useFormState } from 'react-dom'
 
 type Props = {}
 
 const BoardsForm = (props: Props) => {
+	const initialState = { message: null, errors: {} }
+	const [state, dispatch] = useFormState(create, initialState)
+
 	return (
-		<form action={create}>
+		<form action={dispatch}>
 			<input
 				id='title'
 				name='title'
