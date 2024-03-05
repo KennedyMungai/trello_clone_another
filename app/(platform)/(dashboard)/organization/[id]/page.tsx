c890dec1e50639c32634cbdce6aa8c2890de3553
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 14:44:01 +0300

    Added a separator for the organization info

--- app/(platform)/(dashboard)/organization/[id]/page.tsx
+++ app/(platform)/(dashboard)/organization/[id]/page.tsx
@@ -1,8 +1,10 @@
+import { Separator } from "@/components/ui/separator"
 import Info from "./_components/info"
 
 const SingleOrganizationPage = async () => {
 	return <div className='w-full mb-2'>
 		<Info />
+		<Separator className="my-4" />
 	</div>
 }
 
