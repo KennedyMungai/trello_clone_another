c70d031b2fa72b5592b3e6af8d9a2b942b93dedd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 15:22:16 +0300

    Gave the formPopover a sideoffset of 10 and a side of right

--- app/(platform)/(dashboard)/organization/[id]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[id]/_components/board-list.tsx
@@ -12,7 +12,7 @@ const BoardList = (props: Props) => {
 				Your Boards
 			</div>
 			<div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-'>
-				<FormPopOver>
+				<FormPopOver sideOffset={10} side='right'>
 					<div
 						role='button'
 						className='aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition'
