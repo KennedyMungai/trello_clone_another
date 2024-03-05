b0e7571d093c9927fb435d80213da766955e78de
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 10:51:01 +0300

    Extracted the pending prop from the formButtonAnother component

--- app/(platform)/(dashboard)/organization/[id]/formButtonAnother.tsx
+++ app/(platform)/(dashboard)/organization/[id]/formButtonAnother.tsx
@@ -1,10 +1,13 @@
 import { Button } from '@/components/ui/button'
+import { useFormStatus } from 'react-dom'
 
 type Props = {}
 
 const FormButtonAnother = (props: Props) => {
+	const { pending } = useFormStatus()
+
 	return (
-		<Button type='submit' className='ml-2' size={'sm'}>
+		<Button type='submit' className='ml-2' size={'sm'} disabled={pending}>
 			Submit
 		</Button>
 	)
