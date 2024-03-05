382bbc456773e4844bc4b82e9e547834b78a94e3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 10:49:53 +0300

    Created another formButton

---
+++ app/(platform)/(dashboard)/organization/[id]/formButtonAnother.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+
+type Props = {}
+
+const FormButtonAnother = (props: Props) => {
+	return (
+		<Button type='submit' className='ml-2' size={'sm'}>
+			Submit
+		</Button>
+	)
+}
+
+export default FormButtonAnother
