6b97399b630b8d3fb2caca238613b9404a949dd0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 14:48:51 +0300

    Added some proper identifying markup for a board

--- app/(platform)/(dashboard)/organization/[id]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[id]/_components/board-list.tsx
@@ -1,7 +1,16 @@
+import { User2 } from 'lucide-react'
+
 type Props = {}
 
 const BoardList = (props: Props) => {
-	return <div>BoardList</div>
+	return (
+		<div className='space-y-4'>
+			<div className='flex items-center font-semibold text-lg text-neutral-700'>
+				<User2 className='h-6 w-6 mr-2' />
+				Your Boards
+			</div>
+		</div>
+	)
 }
 
 export default BoardList
