a23e5ed2862bde8b0bb9a1aa235108478a4d756c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 14:24:08 +0300

    Updated and deleted components

--- app/(platform)/(dashboard)/organization/[id]/page.tsx
+++ app/(platform)/(dashboard)/organization/[id]/page.tsx
@@ -1,23 +1,7 @@
-import { db } from '@/lib/db'
-import BoardsForm from './form'
-
 type Props = {}
 
 const SingleOrganizationPage = async (props: Props) => {
-	const boards = await db.board.findMany({})
-
-	return (
-		<div className='flex flex-col space-y-4'>
-			<BoardsForm />
-			<div className='space-y-2'>
-				{boards.map((board, index) => (
-					<div className='' key={index}>
-						Board Title: {board.title}
-					</div>
-				))}
-			</div>
-		</div>
-	)
+	return <div className='flex flex-col space-y-4'></div>
 }
 
 export default SingleOrganizationPage
