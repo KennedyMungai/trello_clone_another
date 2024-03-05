5aee507ee1886a6063f0c3c0b1f1b31eb2f167ac
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 09:49:46 +0300

    Defined the ts file to hold the logic to create a board

---
+++ actions/createBoard.ts
@@ -0,0 +1,13 @@
+import { db } from "@/lib/db"
+
+export const create = async (formData: FormData) => {
+    'use server'
+
+    const title = formData.get('title') as string
+
+    await db.board.create({
+        data: {
+            title
+        }
+    })
+}
