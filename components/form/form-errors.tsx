43d651fe28023b0897ec7f8484a0ca4c86b04830
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 12:39:31 +0300

    Added some markup, styles and logic

--- components/form/form-errors.tsx
+++ components/form/form-errors.tsx
@@ -1,9 +1,32 @@
 'use client'
 
-type Props = {}
+import { XCircleIcon } from 'lucide-react'
 
-const FormErrors = (props: Props) => {
-	return <div>FormErrors</div>
+type Props = {
+	id: string
+	errors?: Record<string, string[] | undefined>
+}
+
+const FormErrors = ({ errors, id }: Props) => {
+	if (!errors) return null
+
+	return (
+		<div
+			id={`${id}-error`}
+			aria-live={'polite'}
+			className='mt-2 text-xs text-rose-500'
+		>
+			{errors?.[id]?.map((error: string, index) => (
+				<div
+					className='flex items-center font-medium p-2 border border-rose-500 bg-rose-500/10 rounded-sm'
+					key={index}
+				>
+					<XCircleIcon className='h-4 w-4 mr-2' />
+					{error}
+				</div>
+			))}
+		</div>
+	)
 }
 
 export default FormErrors
