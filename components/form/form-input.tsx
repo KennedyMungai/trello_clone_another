68dae68ef63550369e8336717aae63e686020d1e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 12:22:44 +0300

    Created the form input component

--- components/form/form-input.tsx
+++ components/form/form-input.tsx
@@ -3,6 +3,8 @@
 import { forwardRef } from 'react'
 import { useFormStatus } from 'react-dom'
 import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+import { cn } from '@/lib/utils'
 
 interface FormInputProps {
 	id: string
@@ -37,9 +39,33 @@ export const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
 
 		return (
 			<div className='space-y-2'>
-				<Label className='space-y-1'>
-					{label ? <div>Label</div> : null}
-				</Label>
+				<div className='space-y-1'>
+					{label ? (
+						<Label
+							htmlFor={id}
+							className='text-xs font-semibold text-neutral-700'
+						>
+							Label
+						</Label>
+					) : null}
+					<Input
+						onBlur={onBlur}
+						defaultValue={defaultValue}
+						ref={ref}
+						required={required}
+						name={id}
+						id={id}
+						placeholder={placeholder}
+						type={type}
+						disabled={pending || disabled}
+						className={cn('text-sm px-2 py-1 h-7', className)}
+						aria-describedby={`${id}-error`}
+					/>
+				</div>
+                <FormErrors 
+                    id={id}
+                    errors={errors}
+                />
 			</div>
 		)
 	}
