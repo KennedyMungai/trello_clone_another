8991bbec6c7971912ad60f838c175b8ad52f593c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 09:58:32 +0300

    Implemented some basic zod validation

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -1,8 +1,14 @@
 'use server'
+
 import { db } from '@/lib/db'
+import { z } from 'zod'
+
+const CreateBoard = z.object({
+	title: z.string()
+})
 
 export const create = async (formData: FormData) => {
-	const title = formData.get('title') as string
+	const { title } = CreateBoard.parse({ title: formData.get('title') })
 
 	await db.board.create({
 		data: {
