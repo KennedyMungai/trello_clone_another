5158e432a3f6a77db0f0871efbb0098421e33828
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 12:56:12 +0300

    Created the form-submit component

--- components/form/form-submit.tsx
+++ components/form/form-submit.tsx
@@ -1,9 +1,38 @@
 'use client'
 
-type Props = {}
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import { ReactNode } from 'react'
+import { useFormStatus } from 'react-dom'
 
-const FormSubmit = (props: Props) => {
-	return <div>FormButton</div>
+type Props = {
+	children: ReactNode
+	disabled?: boolean
+	className?: string
+	variant?:
+		| 'default'
+		| 'destructive'
+		| 'outline'
+		| 'secondary'
+		| 'ghost'
+		| 'link'
+		| 'primary'
+}
+
+const FormSubmit = ({ children, className, disabled, variant }: Props) => {
+	const { pending } = useFormStatus()
+
+	return (
+		<Button
+			disabled={pending || disabled}
+			type='submit'
+			variant={variant}
+			size='sm'
+			className={cn(className)}
+		>
+			{children}
+		</Button>
+	)
 }
 
 export default FormSubmit
