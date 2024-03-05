f4fbfc8ec3ed9f14d61b76b50218394f2b256fa1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 10:21:25 +0300

    Created the State type

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -4,6 +4,13 @@ import { db } from '@/lib/db'
 import { revalidatePath } from 'next/cache'
 import { z } from 'zod'
 
+export type State = {
+	errors?: {
+		title?: string
+	}
+	message: string | null
+}
+
 const CreateBoard = z.object({
 	title: z
 		.string()
