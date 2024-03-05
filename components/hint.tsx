834e8984746572df323e8c7fde3bfc012d12248b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-05 15:04:37 +0300

    Finished writing the hint component

--- components/hint.tsx
+++ components/hint.tsx
@@ -1,9 +1,40 @@
 'use client'
 
-type Props = {}
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger
+} from '@/components/ui/tooltip'
+import { ReactNode } from 'react'
 
-const Hint = (props: Props) => {
-	return <div>hint</div>
+type Props = {
+	children: ReactNode
+	description: string
+	side?: 'top' | 'bottom' | 'left' | 'right'
+	sideOffset?: number
+}
+
+const Hint = ({
+	children,
+	description,
+	side = 'bottom',
+	sideOffset = 0
+}: Props) => {
+	return (
+		<TooltipProvider>
+			<Tooltip delayDuration={0}>
+				<TooltipTrigger>{children}</TooltipTrigger>
+				<TooltipContent
+					side={side}
+					sideOffset={sideOffset}
+					className='text-xs max-w-[220px] break-words'
+				>
+					{description}
+				</TooltipContent>
+			</Tooltip>
+		</TooltipProvider>
+	)
 }
 
 export default Hint
