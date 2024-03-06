3ab7af7c900d2bfe205eaa4d5a08a03269726fe4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-06 14:53:01 +0300

    Added some padding for the text

--- app/(platform)/(dashboard)/organization/[id]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[id]/_components/board-list.tsx
@@ -39,7 +39,7 @@ const BoardList = async () => {
 						className='group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden'
 					>
 						<div className='absolute inset-0 bg-black/30 group-hover:bg-black/40 transition'>
-							<p className='relative font-semibold text-white'>
+							<p className='relative font-semibold text-white p-2'>
 								{board.title}
 							</p>
 						</div>
