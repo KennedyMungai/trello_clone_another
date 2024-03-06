e61149ac574c37e5254fcc88a5efbd03359ae042
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-06 12:14:14 +0300

    Styled the titles of the cards representing the boards

--- app/(platform)/(dashboard)/organization/[id]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[id]/_components/board-list.tsx
@@ -3,6 +3,7 @@ import Hint from '@/components/hint'
 import { db } from '@/lib/db'
 import { auth } from '@clerk/nextjs'
 import { HelpCircle, User2 } from 'lucide-react'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 const BoardList = async () => {
@@ -27,7 +28,23 @@ const BoardList = async () => {
 				<User2 className='h-6 w-6 mr-2' />
 				Your Boards
 			</div>
-			<div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-'>
+			<div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4'>
+				{boards.map((board, index) => (
+					<Link
+						key={index}
+						href={`/board/${board.id}`}
+						style={{
+							backgroundImage: `url(${board.imageThumbUrl})`
+						}}
+						className='group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden'
+					>
+						<div className='absolute inset-0 bg-black/30 group-hover:bg-black/40 transition'>
+							<p className='relative font-semibold text-white'>
+								{board.title}
+							</p>
+						</div>
+					</Link>
+				))}
 				<FormPopOver sideOffset={10} side='right'>
 					<div
 						role='button'
