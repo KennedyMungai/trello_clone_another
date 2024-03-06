ce61af90d8cd845298be941b851a00d27a48762f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-06 14:56:38 +0300

    Created a Skeleton component for the board list

--- app/(platform)/(dashboard)/organization/[id]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[id]/_components/board-list.tsx
@@ -1,5 +1,6 @@
 import FormPopOver from '@/components/form/form-popover'
 import Hint from '@/components/hint'
+import { Skeleton } from '@/components/ui/skeleton'
 import { db } from '@/lib/db'
 import { auth } from '@clerk/nextjs'
 import { HelpCircle, User2 } from 'lucide-react'
@@ -70,3 +71,18 @@ const BoardList = async () => {
 }
 
 export default BoardList
+
+BoardList.Skeleton = function SkeletonBoardList() {
+	return (
+		<div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4'>
+			<Skeleton className='aspect-video h-full w-full p-2' />
+			<Skeleton className='aspect-video h-full w-full p-2' />
+			<Skeleton className='aspect-video h-full w-full p-2' />
+			<Skeleton className='aspect-video h-full w-full p-2' />
+			<Skeleton className='aspect-video h-full w-full p-2' />
+			<Skeleton className='aspect-video h-full w-full p-2' />
+			<Skeleton className='aspect-video h-full w-full p-2' />
+			<Skeleton className='aspect-video h-full w-full p-2' />
+		</div>
+	)
+}
