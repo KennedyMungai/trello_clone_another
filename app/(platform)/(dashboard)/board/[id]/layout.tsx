1d11eb8931a9dd2d15227d70e7765854b4494014
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-06 15:21:56 +0300

    Added a background picture to the individual board page. The same pic as the avatar on the board list only bigger

--- app/(platform)/(dashboard)/board/[id]/layout.tsx
+++ app/(platform)/(dashboard)/board/[id]/layout.tsx
@@ -1,11 +1,37 @@
+import { db } from '@/lib/db'
+import { auth } from '@clerk/nextjs'
+import { notFound, redirect } from 'next/navigation'
 import { ReactNode } from 'react'
 
 type Props = {
 	children: ReactNode
+	params: {
+		id: string
+	}
 }
 
-const SingleBoardLayoutPage = ({ children }: Props) => {
-	return <div>{children}</div>
+const SingleBoardLayoutPage = async ({ children, params: { id } }: Props) => {
+	const { orgId } = auth()
+
+	if (!orgId) redirect('/select-org')
+
+	const board = await db.board.findUnique({
+		where: {
+			id,
+			orgId
+		}
+	})
+
+	if (!board) notFound()
+
+	return (
+		<div
+			style={{ backgroundImage: `url(${board.imageFullUrl})` }}
+			className='relative h-full bg-no-repeat bg-cover bg-center'
+		>
+			<main className='relative pt-28 h-full'>{children}</main>
+		</div>
+	)
 }
 
 export default SingleBoardLayoutPage
