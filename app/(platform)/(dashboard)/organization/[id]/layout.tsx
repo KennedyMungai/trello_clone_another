d3b24245520e421e4e87e76dce8e41569c46a5ed
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-06 15:07:47 +0300

    Implemented logic to generate metadata for a specific page

--- app/(platform)/(dashboard)/organization/[id]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[id]/layout.tsx
@@ -1,5 +1,15 @@
+import { auth } from '@clerk/nextjs'
+import { startCase } from 'lodash'
 import { ReactNode } from 'react'
 import OrgControl from './_components/OrgControl'
+
+export const generateMetadata = () => {
+	const { orgSlug } = auth()
+
+	return {
+		title: startCase(orgSlug || 'organization')
+	}
+}
 
 type Props = {
 	children: ReactNode
