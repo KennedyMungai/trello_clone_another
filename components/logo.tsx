832111313fda5c34a241b62d39ab2618072be804
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-07 09:53:28 +0300

    Updated the location of the trello svg file

--- components/logo.tsx
+++ components/logo.tsx
@@ -7,8 +7,10 @@ const Logo = (props: Props) => {
 	return (
 		<Link href={'/'}>
 			<div className='hover:opacity-75 transition items-center gap-x-2 hidden md:flex'>
-				<Image src={'trello.svg'} alt='Logo' width={30} height={30} />
-				<p className='text-lg text-neutral-700 pb-1 font-bold'>Trello Clone</p>
+				<Image src={'/trello.svg'} alt='Logo' width={30} height={30} />
+				<p className='text-lg text-neutral-700 pb-1 font-bold'>
+					Trello Clone
+				</p>
 			</div>
 		</Link>
 	)
