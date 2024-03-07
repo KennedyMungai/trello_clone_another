f53b7912668bad403dcc1e9592497d5a413915a8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-07 11:11:33 +0300

    Created the list wrapper component

---
+++ app/(platform)/(dashboard)/board/[id]/_components/list-wrapper.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react'
+
+type Props = {
+	children: ReactNode
+}
+
+const ListWrapper = ({ children }: Props) => {
+	return <li className='shrink-0 h-full w-[272px] select-none'>{children}</li>
+}
+
+export default ListWrapper
