647b386fd66bfb011c010c36cc844e0d506f2e2c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-07 12:32:07 +0300

    Rendered out the list title on a card

--- app/(platform)/(dashboard)/board/[id]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[id]/_components/list-header.tsx
@@ -1,9 +1,19 @@
 'use client'
 
-type Props = {}
+import { List } from 'postcss/lib/list'
 
-const ListHeader = (props: Props) => {
-	return <div>ListHeader</div>
+type Props = {
+	data: List
+}
+
+const ListHeader = ({ data }: Props) => {
+	return (
+		<div className='pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2'>
+			<div className='w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent'>
+				{data.title}
+			</div>
+		</div>
+	)
 }
 
 export default ListHeader
--- app/(platform)/(dashboard)/board/[id]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[id]/_components/list-item.tsx
@@ -12,7 +12,7 @@ const ListItem = ({ data, index }: Props) => {
 	return (
 		<li className='shrink-0 h-full w-[272px] select-none'>
 			<div className='w-full rounded-md bg-[#f1f2f4] shadow-md pb-2'>
-				<ListHeader />
+				<ListHeader data={data} />
 			</div>
 		</li>
 	)
