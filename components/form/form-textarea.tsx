732442b8e2bec96f8b3269e3ff07fc38bbb82dbb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-07 16:10:02 +0300

    Made some changes

--- components/form/form-textarea.tsx
+++ components/form/form-textarea.tsx
@@ -1,6 +1,11 @@
 'use client'
 
+import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
+import { cn } from '@/lib/utils'
 import { KeyboardEventHandler, forwardRef } from 'react'
+import { useFormStatus } from 'react-dom'
+import FormErrors from './form-errors'
 
 type Props = {
 	id: string
@@ -33,7 +38,40 @@ const FormTextarea = forwardRef<HTMLFormElement, Props>(
 		},
 		ref
 	) => {
-		return <div>FormTextArea</div>
+		const { pending } = useFormStatus()
+
+		return (
+			<div className='space-y-2 w-full'>
+				<div className='space-y-1 w-full'>
+					{label ? (
+						<Label
+							htmlFor={id}
+							className={'text-xs font-semibold text-neutral-700'}
+						>
+							{label}
+						</Label>
+					) : null}
+					<Textarea
+						onKeyDown={onKeyDown}
+						onBlur={onBlur}
+						onClick={onClick}
+						ref={ref}
+						required={required}
+						placeholder={placeholder}
+						name={id}
+						id={id}
+						disabled={disabled || pending}
+						className={cn(
+							'resize-none focus-visible:ring-0 focus-visible:ring-offset-0 ring-0 focus:ring-0 outline-none shadow-sm',
+							className
+						)}
+						aria-describedby={`${id}-error`}
+						defaultValue={defaultValue}
+					/>
+				</div>
+				<FormErrors id={id} errors={errors} />
+			</div>
+		)
 	}
 )
 
