271cd347030314e4d8fe3944318c1c3e69cf19d5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-07 12:05:18 +0300

    Implemented some list form logic to create a new list title

--- app/(platform)/(dashboard)/board/[id]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[id]/_components/list-form.tsx
@@ -1,13 +1,16 @@
 'use client'
 
+import { createList } from '@/actions/list/create-list'
 import { FormInput } from '@/components/form/form-input'
-import { Plus, X } from 'lucide-react'
-import { ElementRef, KeyboardEvent, useRef, useState } from 'react'
-import { useEventListener, useOnClickOutside } from 'usehooks-ts'
-import ListWrapper from './list-wrapper'
-import { useParams } from 'next/navigation'
 import FormSubmit from '@/components/form/form-submit'
 import { Button } from '@/components/ui/button'
+import { useAction } from '@/hooks/use-action'
+import { Plus, X } from 'lucide-react'
+import { useParams, useRouter } from 'next/navigation'
+import { ElementRef, KeyboardEvent, useRef, useState } from 'react'
+import { toast } from 'sonner'
+import { useEventListener, useOnClickOutside } from 'usehooks-ts'
+import ListWrapper from './list-wrapper'
 
 type Props = {}
 
@@ -18,6 +21,8 @@ const ListForm = (props: Props) => {
 	const inputRef = useRef<ElementRef<'input'>>(null)
 
 	const { id } = useParams()
+
+	const router = useRouter()
 
 	const enableEditing = () => {
 		setIsEditing(true)
@@ -31,6 +36,17 @@ const ListForm = (props: Props) => {
 		setIsEditing(false)
 	}
 
+	const { execute, fieldErrors } = useAction(createList, {
+		onSuccess: (data) => {
+			toast.success(`List ${data.title} created`)
+			disableEditing()
+			router.refresh()
+		},
+		onError: (error) => {
+			toast.error(error)
+		}
+	})
+
 	const onKeyDown = (e: KeyboardEvent) => {
 		if (e.key === 'Escape') {
 			disableEditing()
@@ -40,16 +56,24 @@ const ListForm = (props: Props) => {
 	useEventListener('keydown', onKeyDown)
 	useOnClickOutside(formRef, disableEditing)
 
+	const onSubmit = (formData: FormData) => {
+		const title = formData.get('title') as string
+		const boardId = formData.get('boardId') as string
+
+		execute({ title, boardId })
+	}
+
 	if (isEditing) {
 		return (
 			<ListWrapper>
 				<form
+					action={onSubmit}
 					ref={formRef}
 					className='w-full p-3 rounded-md bg-white space-y-4 shadow-md'
 				>
 					<FormInput
 						id={'title'}
-						errors={undefined}
+						errors={fieldErrors}
 						ref={inputRef}
 						className={
 							'text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition'
