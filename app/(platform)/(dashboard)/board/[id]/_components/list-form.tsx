7524f439af851e1e208566f786b2a9ce7314f6f0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-07 11:16:55 +0300

    Added some markup to the component

--- app/(platform)/(dashboard)/board/[id]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[id]/_components/list-form.tsx
@@ -1,9 +1,19 @@
 'use client'
 
+import { Plus } from 'lucide-react'
+import ListWrapper from './list-wrapper'
+
 type Props = {}
 
 const ListForm = (props: Props) => {
-	return <div>ListForm</div>
+	return (
+		<ListWrapper>
+			<button className='w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex items-center font-medium text-sm'>
+				<Plus className='h-4 w-4 mr-2' />
+				Add A List
+			</button>
+		</ListWrapper>
+	)
 }
 
 export default ListForm
