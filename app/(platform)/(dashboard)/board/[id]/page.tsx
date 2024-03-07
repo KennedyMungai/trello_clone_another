146fd7a9be1cf8b882a47529ede80969fd52da27
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-07 10:49:42 +0300

    Got the lists and their cards

--- app/(platform)/(dashboard)/board/[id]/page.tsx
+++ app/(platform)/(dashboard)/board/[id]/page.tsx
@@ -1,6 +1,32 @@
-type Props = {}
+import { db } from '@/lib/db'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 
-const SingleBoardPage = (props: Props) => {
+type Props = {
+	params: { id: string }
+}
+
+const SingleBoardPage = async ({ params: { id } }: Props) => {
+	const { orgId } = auth()
+
+	if (!orgId) redirect('/select-org')
+
+	const lists = await db.list.findMany({
+		where: {
+			boardId: id,
+			board: {
+				orgId
+			}
+		},
+		include: {
+			cards: {
+				orderBy: {
+					order: 'asc'
+				}
+			}
+		}
+	})
+
 	return <div>SingleBoardPage</div>
 }
 
