7c0d067852fa6c20bd66c1d8594df26ad15d80f4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-07 15:48:56 +0300

    Added some markup to the card form component

--- app/(platform)/(dashboard)/board/[id]/_components/card-form.tsx
+++ app/(platform)/(dashboard)/board/[id]/_components/card-form.tsx
@@ -1,5 +1,9 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
+import { Plus } from 'lucide-react'
+import { forwardRef } from 'react'
+
 type Props = {
 	isEditing: boolean
 	enableEditing: () => void
@@ -7,13 +11,26 @@ type Props = {
 	listId: string
 }
 
-const CardForm = ({
-	disableEditing,
-	enableEditing,
-	isEditing,
-	listId
-}: Props) => {
-	return <div>CardForm</div>
-}
+const CardForm = forwardRef<HTMLTextAreaElement, Props>(
+	({ disableEditing, enableEditing, isEditing, listId }, ref) => {
+		return (
+			<div className='pt-2 px-2'>
+				<Button
+					onClick={enableEditing}
+					className={
+						'h-auto px-2 py-1.5 w-full justify-start text-muted-foreground text-sm'
+					}
+					size='sm'
+					variant='ghost'
+				>
+					<Plus className='w-4 h-4 mr-4' />
+					Add a card
+				</Button>
+			</div>
+		)
+	}
+)
+
+CardForm.displayName = 'CardForm'
 
 export default CardForm
