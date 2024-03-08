cc3c387e1b8cd6165c001cfe755e6d0113d9c703
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-08 09:48:07 +0300

    Some draggability shenanigans

--- app/(platform)/(dashboard)/board/[id]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[id]/_components/card-item.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Card } from '@prisma/client'
+import { Draggable } from '@hello-pangea/dnd'
 
 type Props = {
 	index: number
@@ -9,12 +10,19 @@ type Props = {
 
 const CardItem = ({ index, data }: Props) => {
 	return (
-		<div
-			role={'button'}
-			className='truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm'
-		>
-			{data.title}
-		</div>
+		<Draggable draggableId={data.id} index={index}>
+			{(provided) => (
+				<div
+					role={'button'}
+					className='truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm'
+					ref={provided.innerRef}
+					{...provided.draggableProps}
+					{...provided.dragHandleProps}
+				>
+					{data.title}
+				</div>
+			)}
+		</Draggable>
 	)
 }
 
