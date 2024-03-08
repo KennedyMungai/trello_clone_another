0123cf0f26ea96c81bed5249f6cd44b06330a271
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-08 09:35:38 +0300

    Setup the drag drop logic for the list items

--- app/(platform)/(dashboard)/board/[id]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[id]/_components/list-container.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { ListWithCards } from '@/types'
+import { DragDropContext, Droppable } from '@hello-pangea/dnd'
 import { useEffect, useState } from 'react'
 import ListForm from './list-form'
 import ListItem from './list-item'
@@ -18,13 +19,19 @@ const ListContainer = ({ boardId, data }: Props) => {
 	}, [data])
 
 	return (
-		<ol className='flex gap-x-3 h-full'>
-			{orderedData.map((list, index) => (
-				<ListItem key={list.id} data={list} index={index} />
-			))}
-			<ListForm />
-			<div className='flex-shrink-0 w-1' />
-		</ol>
+		<DragDropContext onDragEnd={() => {}}>
+			<Droppable droppableId='lists' type='list' direction='horizontal'>
+				{(provided) => (
+					<ol className='flex gap-x-3 h-full'>
+						{orderedData.map((list, index) => (
+							<ListItem key={list.id} data={list} index={index} />
+						))}
+						<ListForm />
+						<div className='flex-shrink-0 w-1' />
+					</ol>
+				)}
+			</Droppable>
+		</DragDropContext>
 	)
 }
 
