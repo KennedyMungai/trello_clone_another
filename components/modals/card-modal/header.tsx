4a051ba46414148793c3be212b37dbfc5c9130bd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-11 11:04:56 +0300

    Made some changes

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -3,8 +3,10 @@
 import { FormInput } from "@/components/form/form-input"
 import { Skeleton } from "@/components/ui/skeleton"
 import { CardWithList } from "@/types"
+import { useQueryClient } from "@tanstack/react-query"
 import { Layout } from "lucide-react"
-import { useState } from "react"
+import { useParams } from "next/navigation"
+import { ElementRef, useRef, useState } from "react"
 
 type Props = {
     data: CardWithList
@@ -13,17 +15,36 @@ type Props = {
 const Header = ({ data }: Props) => {
     const [title, setTitle] = useState(data.title)
 
+    const queryClient = useQueryClient()
+
+    const { id } = useParams()
+
+    const inputRef = useRef<ElementRef<"input">>(null)
+
+    const onBlur = () => {
+        inputRef.current?.form?.requestSubmit()
+    }
+
+    const onSubmit = (formData: FormData) => {
+        console.log(formData.get('title')) as string
+    }
+
     return (
         <div className="flex items-start gap-x-3 mb-6 w-full">
             <Layout className="h-5 w-5 mt-1 text-neutral-700" />
             <div className="w-full">
-                <form>
+                <form action={onSubmit}>
                     <FormInput
                         id={"title"}
                         defaultValue={title}
+                        onBlur={onBlur}
+                        ref={inputRef}
                         className="font-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
                     />
                 </form>
+                <p className='text-sm text-muted-foreground'>
+                    In list <span className='underline'>{data.list.title}</span>
+                </p>
             </div>
         </div>
     )
