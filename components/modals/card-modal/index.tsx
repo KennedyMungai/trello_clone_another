9f6962a8226d0f1b62d874f10b29457958b831c4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-11 11:31:45 +0300

    Rendered out the Description component

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -5,6 +5,7 @@ import { useCardModal } from "@/hooks/use-card-modal"
 import { fetcher } from "@/lib/fetcher"
 import { CardWithList } from "@/types"
 import { useQuery } from "@tanstack/react-query"
+import Description from "./description"
 import Header from "./header"
 
 type Props = {}
@@ -25,6 +26,17 @@ const CardModal = (props: Props) => {
                         ? (<Header data={cardData} />)
                         : <Header.Skeleton />
                 }
+                <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+                    <div className="col-span-3">
+                        <div className="w-full space-y-6">
+                            {
+                                cardData
+                                    ? (<Description data={cardData} />)
+                                    : (<Description.Skeleton />)
+                            }
+                        </div>
+                    </div>
+                </div>
             </DialogContent>
         </Dialog>
     )
