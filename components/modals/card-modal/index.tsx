44401facef663f77906a859eec3e53694c1b084d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-11 10:37:39 +0300

    Added some custom data to the modal

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -2,16 +2,24 @@
 
 import { Dialog, DialogContent } from "@/components/ui/dialog"
 import { useCardModal } from "@/hooks/use-card-modal"
+import { fetcher } from "@/lib/fetcher"
+import { CardWithList } from "@/types"
+import { useQuery } from "@tanstack/react-query"
 
 type Props = {}
 
 const CardModal = (props: Props) => {
     const { id, isOpen, onClose, onOpen } = useCardModal((state) => state)
 
+    const { data: cardData } = useQuery<CardWithList>({
+        queryKey: ['card', id],
+        queryFn: () => fetcher(`/api/cards/${id}`)
+    })
+
     return (
         <Dialog open={isOpen} onOpenChange={onClose}>
             <DialogContent>
-                I am a modal
+                {cardData?.title}
             </DialogContent>
         </Dialog>
     )
