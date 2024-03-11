3e2f848a51831fb72e459e53a5ab6f4df99f2768
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-11 10:17:54 +0300

    Created the query client provider

---
+++ components/providers/query-provider.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { ReactNode, useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+type Props = {
+    children: ReactNode
+}
+
+const QueryProvider = ({ children }: Props) => {
+    const [queryClient] = useState(() => new QueryClient())
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+}
+
+export default QueryProvider
