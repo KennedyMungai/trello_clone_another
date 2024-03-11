3230f212d2536125f8c85e64f1478bf4678baef8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-11 11:41:58 +0300

    Added some markup

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -2,6 +2,7 @@
 
 import { Skeleton } from "@/components/ui/skeleton"
 import { CardWithList } from "@/types"
+import { AlignLeft } from "lucide-react"
 
 type Props = {
     data: CardWithList
@@ -9,7 +10,16 @@ type Props = {
 
 const Description = ({ data }: Props) => {
     return (
-        <div>{data.description}</div>
+        <div className="flex items-start gap-x-3 w-full">
+            <AlignLeft className="h-5 w-5 mt-0.5 text-neutral-700" />
+            <div className="w-full">
+                <p className="font-semibold text-neutral-700 mb-2">Description</p>
+                <div role='button' className='min-h-[78px] bg-neutral-200 text-sm font-medium py-3 px-3.5 rounded-md'>
+                    {data.description || "Add a more detailed description"}
+                </div>
+            </div>
+            {data.description}
+        </div>
     )
 }
 
